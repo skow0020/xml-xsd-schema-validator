2a652e396557a4ed961aac19d2cc0e090386f973
Colin Skow <cskow@tapqa.com>
2018-10-04 14:19:41 -0500

    Format fixes

--- validatorScript.js
+++ validatorScript.js
@@ -4,27 +4,27 @@ const fs = require('fs');
 const path = require('path');
 
 const testFolder = './xmls/';
-const resultsFile = 'results.csv'
+const resultsFile = 'results.csv';
 
 const files = fs.readdirSync('./');
-const xsdFiles = files.filter(function(file) {
-    return path.extname(file).toLowerCase() === '.xsd';
+const xsdFiles = files.filter(function (file) {
+  return path.extname(file).toLowerCase() === '.xsd';
 });
-if (xsdFiles.length != 1 ) { throw error('There must be exactly 1 xsd file in the base directory'); }
+if (xsdFiles.length != 1) { throw 'There must be exactly 1 xsd file in the base directory'; }
 
-fs.writeFileSync(resultsFile, 'FileName, Results, Messages,\n')
+fs.writeFileSync(resultsFile, 'FileName, Results, Messages,\n');
 
 fs.readdir(testFolder, (err, files) => {
   files.forEach(xmlfile => {
-    validator.validateXML({ file: `${testFolder}${xmlfile}` }, xsdFiles[0], function(err, result) {
+    validator.validateXML({ file: `${testFolder}${xmlfile}` }, xsdFiles[0], function (err, result) {
       if (err && !err.message.includes('invalid xml')) {
         throw err;
-      }
-     
+      };
+
       const validity = result.valid ? 'Valid XML' : 'Invalid XML';
-      
+
       const fileResult = `${xmlfile}, ${validity}, ${result.messages}\n`;
-      console.log(fileResult)
+      console.log(fileResult);
       fs.appendFileSync('results.csv', fileResult);
     });
   });
